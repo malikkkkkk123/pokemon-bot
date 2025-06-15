d57658e99865e1455c8b7effc479faeba6d8e506
malikkkkkk123 <malikalthaf33@gmail.com>
2025-06-15 08:37:49 +0700

    update attack dan hp fr fr

--- logic.py
+++ logic.py
@@ -1,5 +1,6 @@
 import aiohttp  # A library for asynchronous HTTP requests
 import random
+import asyncio
 
 class Pokemon:
     pokemons = {}
@@ -8,10 +9,24 @@ class Pokemon:
         self.pokemon_trainer = pokemon_trainer
         self.pokemon_number = random.randint(1, 1000)
         self.name = None
+        self.ability_name = None
+        self.power = random.randint(1, 40)
+        self.hp = random.randint(40, 500)
         if pokemon_trainer not in Pokemon.pokemons:
             Pokemon.pokemons[pokemon_trainer] = self
         else:
-            self = Pokemon.pokemons[pokemon_trainer]
+            self = Pokemon.pokemons[pokemon_trainer]   
+    async def attack(self, enemy):
+        if isinstance(enemy, Wizard):  # Periksa apakah musuh adalah tipe data Penyihir (instance dari kelas Penyihir)
+            kesempatan = random.randint(1,5)
+            if kesempatan == 1:
+                return "Pokemon penyihir menggunakan perisai dalam pertarungan"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Pertarungan @{self.pokemon_trainer} dengan @{enemy.pokemon_trainer} membuat nyawa @{enemy.pokemon_trainer} berkurang menjadi {enemy.hp}"
+        else:
+            enemy.hp = 0
+            return f"@{self.pokemon_trainer} menang melawan @{enemy.pokemon_trainer}!"
 
     async def get_name(self):
         # An asynchronous method to get the name of a pokémon via PokeAPI
@@ -27,8 +42,53 @@ class Pokemon:
     async def info(self):
         # A method that returns information about the pokémon
         if not self.name:
+            self.ability_name = await self.ability()
             self.name = await self.get_name()  # Retrieving a name if it has not yet been uploaded
-        return f"The name of your Pokémon: {self.name}"  # Returning the string with the Pokémon's name
+        return f"The name of your Pokémon: {self.name}, the ability name of your pokemon: {self.ability_name}, and have {self.power} power, with {self.hp} hp."  # Returning the string with the Pokémon's name
 
     async def show_img(self):
         # An asynchronous method to retrieve the URL of a pokémon image via PokeAPI
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API for the request
+        async with aiohttp.ClientSession() as session:  # Opening an HTTP session
+            async with session.get(url) as response:  # Sending a GET request
+                if response.status == 200:
+                    data = await response.json()  # Receiving and decoding JSON response
+                    return data['sprites']['other']['showdown']['front_default']  # Returning a Pokémon's name
+                else:
+                    return "Pikachu"  
+                
+    async def ability(self):
+        # An asynchronous method to retrieve the URL of a pokémon image via PokeAPI
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API for the request
+        async with aiohttp.ClientSession() as session:  # Opening an HTTP session
+            async with session.get(url) as response:  # Sending a GET request
+                if response.status == 200:
+                    data = await response.json()  # Receiving and decoding JSON response
+                    return data['abilities'][0]['ability']['name']  # Returning a Pokémon's name
+                else:
+                    return "Pikachu"  
+                
+class Wizard(Pokemon):
+    async def attack(self, enemy):
+        return await super().attack(enemy)
+
+class Fighter(Pokemon):
+    async def attack(self, enemy):
+        kekuatan_super = random.randint(5,15)
+        self.power += kekuatan_super
+        hasil = await super().attack(enemy)
+        self.power -= kekuatan_super
+        return hasil + f"\nPetarung menggunakan serangan super dengan kekuatan:{kekuatan_super} "
+    
+async def main():
+    wizard = Wizard("username1")
+    fighter = Fighter("username2")
+
+    print(await wizard.info())
+    print()
+    print(await fighter.info())
+    print()
+    print(await fighter.attack(wizard))
+
+if __name__ == '__main__':
+    asyncio.run(main())
